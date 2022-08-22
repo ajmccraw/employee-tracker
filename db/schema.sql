d49c92a393e0ac979ea6ee4bb02402089a759637
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-21 20:25:05 -0500

    roles

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,22 @@ DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS department;
 
 CREATE DATABASE business;
+
+USE business;
+
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    department_id INT,
+    salary DECIMAL NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL,
+    PRIMARY KEY (id)
+);
+
