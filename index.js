7cf39e2d1b8e1797b48e2e7dd7e88096e58e95a2
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-21 19:51:39 -0500

    mysql connection

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
 
+const departmentsArray = [];
+const employeesArray = [];
+const rolesArray = [];
+
+
+
+// name of business
+let company_name = 'The Computer Company';
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: "business"
+  });
 
