1a0bba112ab9b200d595246222afa82b871195a2
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-21 20:36:01 -0500

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO departments (name)
+VALUES 
+("Marketing"), 
+("Customer Service"), 
+("Human Resources"), 
+("Management");
+
+INSERT INTO roles (title, department_id, salary)
+VALUES 
+("Marketer", 1, 40000),
+("Customer Service Rep", 3, 22000),
+("HR Person", 3, 30000),
+("Manager", 4, 50000),
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES
+("Brandon", "Baker", 1),
+("Sandra", "Smith", 2),
+("Trevor", "Trenkins", 6),
+("Debbie", "Banks", 14),
+
+
+
+UPDATE employees SET manager_id = 3 WHERE id = 4;
+UPDATE employees SET manager_id = 1 WHERE id = 5;
+UPDATE employees SET manager_id = 2 WHERE id = 6;
+UPDATE employees SET manager_id = 2 WHERE id = 7;
