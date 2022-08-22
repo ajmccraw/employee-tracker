724a5ab75ae25405cbf4f3b348cc2a6671267bfb
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-08-21 20:06:41 -0500

    questionList choices

--- index.js
+++ index.js
@@ -77,7 +77,153 @@ const options = (data) => {
     }
 };
 
-
+let questionList = () => {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "menuOptions",
+          message: "Make a choice from the list:",
+          choices: [
+            "List of departments",
+            "List of roles",
+            "List of employees",
+            "Enter a new department",
+            "Enter a new role",
+            "Enter a new employee",
+            "Change an employee's role",
+            "Exit",
+          ],
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "What is the new department name?",
+            when: function (data) {
+              if (data.menuOptions == "Enter a new department") {
+                return true;
+              } else {
+                return false;
+              }
+            },
+        },
+        {
+          type: "input",
+          name: "role",
+          message: "What is the name of this new role?",
+          when: function (data) {
+            if (data.menuOptions == "Enter a new role") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Choose a department:",
+          choices: departmentsArray,
+          when: function (data) {
+            if (data.menuOptions == "Enter a new role") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "How much does this new role earn?",
+          when: function (data) {
+            if (data.menuOptions == "Enter a new role") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee's first name?",
+          when: function (data) {
+            if (data.menuOptions == "Enter a new employee") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the last name of the new employee?",
+          when: function (data) {
+            if (data.menuOptions == "Enter a new employee") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "role",
+          message: "What is the role for this new employee?",
+          choices: rolesArray,
+          when: function (data) {
+            if (data.menuOptions == "Enter a new employee") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "manager",
+          message: "Who is the manager for this employee?",
+          choices: employeesArray,
+          when: function (data) {
+            if (data.menuOptions == "Enter a new employee") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "employee",
+          message: "Choose an employee for the new role",
+          choices: employeesArray,
+          when: function (data) {
+            if (data.menuOptions == "Change an employee's role") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "new_role",
+          message: "Choose a new role",
+          choices: rolesArray,
+          when: function (data) {
+            if (data.menuOptions == "Change an employee's role") {
+              return true;
+            } else {
+              return false;
+            }
+          },
+        },
+      ])
+      .then((answers) => options(answers))
+      .catch((err) => console.log(err));
+};
 
 
 
